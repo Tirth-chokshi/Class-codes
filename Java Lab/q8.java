77a6c2208105d3187cd7e6bf16359f9a13c032d2
Tirth <122982506+T-chokshi@users.noreply.github.com>
2023-08-17 21:21:44 +0530

    lab programs

---
+++ Java Lab/q8.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.lang.Math;
+import java.util.Random;
+
+class RandomPlate {
+    public static void main(String args[]) {
+     
+    Scanner sc = new Scanner(System.in);
+    Random r = new Random();
+    
+    System.out.print("Random number-plate: ");
+    for(int i=0;i<3;i++) {
+    char randomLetter = (char) (r.nextInt(26) + 'A');
+    System.out.print(randomLetter);
+    }
+    System.out.printf("%04d%n", r.nextInt(10000));
+    }
+}
