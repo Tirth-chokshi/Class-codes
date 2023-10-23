18b7605d4805c0face5a64281c60e07bb6fcd034
Tirth chokshi <122982506+T-chokshi@users.noreply.github.com>
2023-10-23 19:04:43 +0530

    folder updated

--- Java Lab/q8.java
+++ Java Lab/q8.java
@@ -1,3 +1,5 @@
+
+/*
 import java.util.Scanner;
 import java.lang.Math;
 import java.util.Random;
@@ -15,4 +17,24 @@ class RandomPlate {
     }
     System.out.printf("%04d%n", r.nextInt(10000));
     }
+}
+*/
+
+import java.util.Scanner;
+import java.lang.Math;
+import java.util.Random;
+
+class RandomPlate{
+	public static void main(String args[]){
+	Scanner scan = new Scanner(System.in);
+	Random r = new Random();
+	
+	System.out.println("Random plate: ");
+	
+	for(int i=0; i<3 ;i++) {
+		char randomLetter = (char)( r.nextInt(26) + 'A');
+		System.out.print(randomLetter);
+	}
+	System.out.printf("%04d%n" , r.nextInt(1000));
+	}
 }
